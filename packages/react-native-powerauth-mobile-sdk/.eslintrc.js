77c9e53a0b82218841dfde4436b496e6ea25ecb0
Martin Budinsky <martin.budinsky@wultra.com>
2025-10-06 12:59:20 +0200

    feat: Introduce eslint, prettier and hardened TS setup to RN packages

--- packages/react-native-powerauth-mobile-sdk/.eslintrc.js
+++ packages/react-native-powerauth-mobile-sdk/.eslintrc.js
@@ -3,8 +3,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['@react-native/eslint-config', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  ignorePatterns: ['node_modules/'],
+  ignorePatterns: ['lib/', 'node_modules/'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
+
+
