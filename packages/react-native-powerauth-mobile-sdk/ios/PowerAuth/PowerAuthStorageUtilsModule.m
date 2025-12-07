f8cd280478bbc01fdb8e3995851c8834cf8a7ab5
Martin Budinsky <martin.budinsky@wultra.com>
2025-12-07 18:35:20 +0100

    feat: Implement iOS module with PowerAuthKeychain & base tests

---
+++ packages/react-native-powerauth-mobile-sdk/ios/PowerAuth/PowerAuthStorageUtilsModule.m
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2025 Wultra s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "PowerAuthStorageUtilsModule.h"
+#import "Errors.h"
+#import <PowerAuth2/PowerAuthKeychain.h>
+
+static NSString * const kStorageTypeSecure = @"SECURE";
+static NSString * const kStorageTypeStandard = @"STANDARD";
+static NSString * const kKeychainServiceName = @"com.wultra.powerauth.jssdk.storageutils.secure";
+static NSString * const kUserDefaultsSuiteName = @"com.wultra.powerauth.jssdk.storageutils.standard";
+
+@implementation PowerAuthStorageUtilsModule
+{
+    PowerAuthKeychain * _keychain;
+    NSUserDefaults * _userDefaults;
+}
+
+RCT_EXPORT_MODULE(PowerAuthStorageUtils);
+
++ (BOOL) requiresMainQueueSetup
+{
+    return NO;
+}
+
+- (PowerAuthKeychain *)keychain
+{
+    if (!_keychain) {
+        _keychain = [[PowerAuthKeychain alloc] initWithIdentifier:kKeychainServiceName];
+    }
+    return _keychain;
+}
+
+- (NSUserDefaults *)userDefaults
+{
+    if (!_userDefaults) {
+        _userDefaults = [[NSUserDefaults alloc] initWithSuiteName:kUserDefaultsSuiteName];
+    }
+    return _userDefaults;
+}
+
+// MARK: - JS interface
+
+PAJS_METHOD_START(setString,
+                  PAJS_ARGUMENT(key, NSString*)
+                  PAJS_ARGUMENT(value, NSString*)
+                  PAJS_ARGUMENT(storageType, NSString*))
+{
+    NSString * keyStr = [RCTConvert NSString:key];
+    NSString * valueStr = [RCTConvert NSString:value];
+    NSString * storageTypeStr = [RCTConvert NSString:storageType];
+    
+    if (!keyStr || keyStr.length == 0) {
+        reject(EC_WRONG_PARAMETER, @"Key cannot be empty", nil);
+        return;
+    }
+    
+    if ([storageTypeStr isEqualToString:kStorageTypeSecure]) {
+        NSData * data = [valueStr dataUsingEncoding:NSUTF8StringEncoding];
+        PowerAuthKeychainStoreItemResult result = [[self keychain] addValue:data forKey:keyStr];
+
+        if (result == PowerAuthKeychainStoreItemResult_Ok) {
+            resolve([NSNull null]);
+        } else if (result == PowerAuthKeychainStoreItemResult_Duplicate) {
+            PowerAuthKeychainStoreItemResult updateResult = [[self keychain] updateValue:data forKey:keyStr];
+
+            if (updateResult == PowerAuthKeychainStoreItemResult_Ok) {
+                resolve([NSNull null]);
+            } else {
+                reject(EC_WRONG_PARAMETER, @"Failed to update value in Keychain", nil);
+            }
+        } else {
+            reject(EC_WRONG_PARAMETER, @"Failed to store value in Keychain", nil);
+        }
+    } else if ([storageTypeStr isEqualToString:kStorageTypeStandard]) {
+        [[self userDefaults] setObject:valueStr forKey:keyStr];
+        [[self userDefaults] synchronize];
+
+        resolve([NSNull null]);
+    } else {
+        reject(EC_WRONG_PARAMETER, @"Invalid storage type", nil);
+    }
+}
+PAJS_METHOD_END
+
+PAJS_METHOD_START(getString,
+                  PAJS_ARGUMENT(key, NSString*)
+                  PAJS_ARGUMENT(storageType, NSString*))
+{
+    NSString * keyStr = [RCTConvert NSString:key];
+    NSString * storageTypeStr = [RCTConvert NSString:storageType];
+    
+    if (!keyStr || keyStr.length == 0) {
+        reject(EC_WRONG_PARAMETER, @"Key cannot be empty", nil);
+        return;
+    }
+    
+    if ([storageTypeStr isEqualToString:kStorageTypeSecure]) {
+        NSData * data = [[self keychain] dataForKey:keyStr status:nil];
+        if (data) {
+            NSString * value = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            resolve(value);
+        } else {
+            resolve([NSNull null]);
+        }
+    } else if ([storageTypeStr isEqualToString:kStorageTypeStandard]) {
+        NSString * value = [[self userDefaults] stringForKey:keyStr];
+
+        if (value) {
+            resolve(value);
+        } else {
+            resolve([NSNull null]);
+        }
+    } else {
+        reject(EC_WRONG_PARAMETER, @"Invalid storage type", nil);
+    }
+}
+PAJS_METHOD_END
+
+PAJS_METHOD_START(exists,
+                  PAJS_ARGUMENT(key, NSString*)
+                  PAJS_ARGUMENT(storageType, NSString*))
+{
+    NSString * keyStr = [RCTConvert NSString:key];
+    NSString * storageTypeStr = [RCTConvert NSString:storageType];
+    
+    if (!keyStr || keyStr.length == 0) {
+        reject(EC_WRONG_PARAMETER, @"Key cannot be empty", nil);
+        return;
+    }
+    
+    if ([storageTypeStr isEqualToString:kStorageTypeSecure]) {
+        BOOL exists = [[self keychain] containsDataForKey:keyStr];
+        resolve(@(exists));
+    } else if ([storageTypeStr isEqualToString:kStorageTypeStandard]) {
+        BOOL exists = [[self userDefaults] objectForKey:keyStr] != nil;
+        resolve(@(exists));
+    } else {
+        reject(EC_WRONG_PARAMETER, @"Invalid storage type", nil);
+    }
+}
+PAJS_METHOD_END
+
+PAJS_METHOD_START(remove,
+                  PAJS_ARGUMENT(key, NSString*)
+                  PAJS_ARGUMENT(storageType, NSString*))
+{
+    NSString * keyStr = [RCTConvert NSString:key];
+    NSString * storageTypeStr = [RCTConvert NSString:storageType];
+    
+    if (!keyStr || keyStr.length == 0) {
+        reject(EC_WRONG_PARAMETER, @"Key cannot be empty", nil);
+        return;
+    }
+    
+    if ([storageTypeStr isEqualToString:kStorageTypeSecure]) {
+        BOOL existed = [[self keychain] containsDataForKey:keyStr];
+        [[self keychain] deleteDataForKey:keyStr];
+
+        resolve(@(existed));
+    } else if ([storageTypeStr isEqualToString:kStorageTypeStandard]) {
+        BOOL existed = [[self userDefaults] objectForKey:keyStr] != nil;
+
+        [[self userDefaults] removeObjectForKey:keyStr];
+        [[self userDefaults] synchronize];
+
+        resolve(@(existed));
+    } else {
+        reject(EC_WRONG_PARAMETER, @"Invalid storage type", nil);
+    }
+}
+PAJS_METHOD_END
+
+@end
---
+++ testapp/_tests/PowerAuth_StorageUtils.test.ts
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2025 Wultra s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TestWithActivation } from "./helpers/TestWithActivation";
+
+import { PowerAuthStorageUtils, PowerAuthStorageType, PowerAuthErrorCode } from "react-native-powerauth-mobile-sdk";
+import { expect } from "../src/testbed";
+
+export class PowerAuth_StorageUtilsTest extends TestWithActivation {
+    override shouldCreateActivationBeforeTest(): boolean {
+        return false;
+    }
+
+    private uniqueKey(prefix: string): string {
+        return `${prefix}_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+    }
+
+    async testSecureStorageSetAndGet() {
+        const key = this.uniqueKey("test_secure");
+        const value = "secure_value_123";
+
+        await PowerAuthStorageUtils.setString(key, value, PowerAuthStorageType.SECURE);
+
+        const retrieved = await PowerAuthStorageUtils.getString(key, PowerAuthStorageType.SECURE);
+        expect(retrieved).toBe(value);
+
+        await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.SECURE);
+    }
+
+    async testStandardStorageSetAndGet() {
+        const key = this.uniqueKey("test_standard");
+        const value = "standard_value_456";
+
+        await PowerAuthStorageUtils.setString(key, value, PowerAuthStorageType.STANDARD);
+
+        const retrieved = await PowerAuthStorageUtils.getString(key, PowerAuthStorageType.STANDARD);
+        expect(retrieved).toBe(value);
+
+        await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.STANDARD);
+    }
+
+    async testSecureStorageExists() {
+        const key = this.uniqueKey("test_exists_secure");
+        const value = "exists_test";
+
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.SECURE)).toBe(false);
+
+        await PowerAuthStorageUtils.setString(key, value, PowerAuthStorageType.SECURE);
+
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.SECURE)).toBe(true);
+
+        await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.SECURE);
+    }
+
+    async testStandardStorageExists() {
+        const key = this.uniqueKey("test_exists_standard");
+        const value = "exists_test";
+
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.STANDARD)).toBe(false);
+
+        await PowerAuthStorageUtils.setString(key, value, PowerAuthStorageType.STANDARD);
+
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.STANDARD)).toBe(true);
+
+        await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.STANDARD);
+    }
+
+    async testSecureStorageRemove() {
+        const key = this.uniqueKey("test_remove_secure");
+        const value = "remove_test";
+
+        await PowerAuthStorageUtils.setString(key, value, PowerAuthStorageType.SECURE);
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.SECURE)).toBe(true);
+
+        const removed = await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.SECURE);
+        expect(removed).toBe(true);
+
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.SECURE)).toBe(false);
+
+        const removedAgain = await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.SECURE);
+        expect(removedAgain).toBe(false);
+    }
+
+    async testStandardStorageRemove() {
+        const key = this.uniqueKey("test_remove_standard");
+        const value = "remove_test";
+
+        await PowerAuthStorageUtils.setString(key, value, PowerAuthStorageType.STANDARD);
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.STANDARD)).toBe(true);
+
+        const removed = await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.STANDARD);
+        expect(removed).toBe(true);
+
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.STANDARD)).toBe(false);
+
+        const removedAgain = await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.STANDARD);
+        expect(removedAgain).toBe(false);
+    }
+
+    async testGetNonExistentKey() {
+        const key = this.uniqueKey("non_existent");
+
+        const secureValue = await PowerAuthStorageUtils.getString(key, PowerAuthStorageType.SECURE);
+        expect(secureValue).toBe(undefined);
+
+        const standardValue = await PowerAuthStorageUtils.getString(key, PowerAuthStorageType.STANDARD);
+        expect(standardValue).toBe(undefined);
+    }
+
+
+    async testEmptyStringValue() {
+        const key = this.uniqueKey("test_empty");
+        const emptyValue = "";
+
+        await PowerAuthStorageUtils.setString(key, emptyValue, PowerAuthStorageType.SECURE);
+        
+        expect(await PowerAuthStorageUtils.exists(key, PowerAuthStorageType.SECURE)).toBe(true);
+        
+        expect(await PowerAuthStorageUtils.getString(key, PowerAuthStorageType.SECURE)).toBe(emptyValue);
+
+        await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.SECURE);
+    }
+
+    async testJsonValue() {
+        const key = this.uniqueKey("test_json");
+        const jsonObject = { name: "test", value: 123, nested: { array: [1, 2, 3] } };
+        const jsonValue = JSON.stringify(jsonObject);
+
+        await PowerAuthStorageUtils.setString(key, jsonValue, PowerAuthStorageType.SECURE);
+
+        const retrieved = await PowerAuthStorageUtils.getString(key, PowerAuthStorageType.SECURE);
+        expect(retrieved).toBe(jsonValue);
+        
+        const parsed = JSON.parse(retrieved!);
+        expect(parsed.name).toBe("test");
+        expect(parsed.value).toBe(123);
+        expect(parsed.nested.array.length).toBe(3);
+
+        await PowerAuthStorageUtils.remove(key, PowerAuthStorageType.SECURE);
+    }
+
+    async testEmptyKeyFails() {
+        await expect(async () => {
+            await PowerAuthStorageUtils.setString("", "value", PowerAuthStorageType.SECURE);
+        }).toThrow({ errorCode: PowerAuthErrorCode.WRONG_PARAMETER });
+
+        await expect(async () => {
+            await PowerAuthStorageUtils.getString("", PowerAuthStorageType.SECURE);
+        }).toThrow({ errorCode: PowerAuthErrorCode.WRONG_PARAMETER });
+
+        await expect(async () => {
+            await PowerAuthStorageUtils.exists("", PowerAuthStorageType.SECURE);
+        }).toThrow({ errorCode: PowerAuthErrorCode.WRONG_PARAMETER });
+
+        await expect(async () => {
+            await PowerAuthStorageUtils.remove("", PowerAuthStorageType.SECURE);
+        }).toThrow({ errorCode: PowerAuthErrorCode.WRONG_PARAMETER });
+    }
+
+}
